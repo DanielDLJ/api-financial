e169c1859a2c6f3e9ed8fa96c00ca6988d17e9c9
Daniel Leme Junior <daniel.dlj@hotmail.com>
2024-09-29 22:48:37 -0300

    AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { EncryptionModule } from './encryption/encryption.module';
+import { ConfigModule } from '@nestjs/config';
+import { configuration } from './common/config/configuration';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { CreditCardsModule } from './credit-cards/credit-cards.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    UsersModule,
+    EncryptionModule,
+    PrismaModule,
+    AuthModule,
+    CreditCardsModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+function configSwagger(app: INestApplication) {
+  const configService = app.get(ConfigService);
+
+  const documentBuilder = new DocumentBuilder()
+    .setTitle(configService.getOrThrow<string>('swagger.title'))
+    .setDescription(configService.getOrThrow<string>('swagger.description'))
+    .setContact(
+      configService.getOrThrow<string>('swagger.contact.name'),
+      '',
+      configService.getOrThrow<string>('swagger.contact.email'),
+    )
+    .setVersion(configService.getOrThrow<string>('swagger.version'))
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('swagger', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
+
+  if (configService.getOrThrow<boolean>('swagger.enable')) {
+    configSwagger(app);
+  }
+
+  await app.listen(configService.getOrThrow<number>('port'));
 }
 bootstrap();
