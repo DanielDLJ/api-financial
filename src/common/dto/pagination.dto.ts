26a8f887dc5bcf5f295cdffc3b4dd11755c33653
Daniel Leme Junior <daniel.dlj@hotmail.com>
2024-10-04 22:53:50 -0300

    swagger

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,14 +1,16 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiPropertyOptional({ default: 1 })
   @IsOptional()
   @Type(() => Number)
   @Min(1)
   @ApiProperty({ default: 1 })
   page = 1;
 
+  @ApiPropertyOptional({ default: 10 })
   @IsOptional()
   @Type(() => Number)
   @Min(0)
