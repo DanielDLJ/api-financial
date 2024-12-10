84e30f040f04ffb10c59596f37afc01e9c86d28f
Daniel Leme Junior <daniel.dlj@hotmail.com>
2024-12-10 18:27:20 -0300

    feat: BANK_NOT_FOUND

--- src/banks/service/banks.service.ts
+++ src/banks/service/banks.service.ts
@@ -1,6 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { BanksRepository } from '../repository/banks.repository';
 import { ListAllBanksDto } from '../dto/list-all-banks.dto';
+import { ApiErrorCode } from '@/common/enums/api-error-codes.enum';
+import { ApiException } from '@/common/exceptions/api.exception';
 
 @Injectable()
 export class BanksService {
@@ -10,11 +12,15 @@ export class BanksService {
     return await this.banksRepository.findAll(query);
   }
 
-  async findOne(id: number, showDeleted?: boolean) {
+  async findOne(id: number, showDeleted = false) {
     const bank = await this.banksRepository.findOne(id, showDeleted);
 
     if (!bank) {
-      throw new NotFoundException(`Bank #${id} not found`);
+      throw new ApiException({
+        code: ApiErrorCode.BANK_NOT_FOUND,
+        message: `Bank #${id} not found`,
+        statusCode: HttpStatus.NOT_FOUND,
+      });
     }
     return bank;
   }
