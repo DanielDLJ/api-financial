ae4c6cf6decd4e64e2f956ed9e77210b5d611d2d
Daniel Leme Junior <daniel.dlj@hotmail.com>
2024-12-09 21:35:16 -0300

    feat: Roles

--- src/flags/controller/flags.controller.ts
+++ src/flags/controller/flags.controller.ts
@@ -4,6 +4,8 @@ import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Flag } from '../entities/flag.entity';
 import { ApiOkResponsePaginated } from '../../common/dto/paginated-response.dto';
 import { ListAllFlagsDto } from '../dto/list-all-flags.dto';
+import { Role } from '@prisma/client';
+import { Roles } from '@/common/decorators/roles.decorator';
 
 @Controller('flags')
 @ApiTags('flags')
@@ -13,6 +15,7 @@ export class FlagsController {
   @Get()
   @ApiOperation({ summary: 'List flags' })
   @ApiOkResponsePaginated(Flag)
+  @Roles(Role.ADMIN, Role.USER)
   findAll(@Query() query: ListAllFlagsDto) {
     return this.flagsService.findAll(query);
   }
@@ -20,6 +23,7 @@ export class FlagsController {
   @Get(':id')
   @ApiOperation({ summary: 'Get flag' })
   @ApiOkResponse({ description: 'Flag', type: Flag })
+  @Roles(Role.ADMIN, Role.USER)
   findOne(@Param('id') id: string) {
     return this.flagsService.findOne(+id);
   }
